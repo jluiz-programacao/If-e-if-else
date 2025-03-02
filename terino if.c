e00638a67e69857f318b6a484d712ce4de462022
jluiz-programacao <202503027871@alunos.estacio.br>
2025-03-02 00:22:34 +0000

    treino if if else

--- terino if.c
+++ terino if.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main (){
+    int numero1;
+    int numero2;
+
+    numero1 = 10;
+    numero2 = 55;
+
+    if(numero1 > numero2) {
+        printf("O número 1 é maior que número 2\n");
+
+    }else {
+        printf("O número 1 é menor que o número 2\n");
+
+    }
+
+
+    }
 
